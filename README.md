936c5e1b2b862e88e81bf74d90aa07b0e2b1cc41
Konstantin553 <149433353+Konstantin553@users.noreply.github.com>
2024-02-12 15:32:00 +0200

    message

---
+++ README.md
@@ -0,0 +1,13 @@
+# Weather-Application
+
+A simple weather application using the openweather API.
+
+Steps to run the app:
+
+    1. Go to https://openweathermap.org
+    2. Create a free account
+    3. Navigate to the API keys section
+    4. Generate a key
+    5. Copy that key to clipboard
+    6. Insert that key into the required variable in the script.js file
+    7. Enjoy!
---
+++ script.js
@@ -0,0 +1,41 @@
+const apiKey = "YOUR_API_KEY";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+const errorMessage = document.querySelector(".error");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    errorMessage.style.display = "block";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/cloudy.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/sun.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rainy-day.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/fog.png";
+    }
+
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchButton.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
